84a157edb1de793c46c60008d5cef190d3aafbc5
FanTerGM <88437276+FanTerGM@users.noreply.github.com>
2023-06-20 22:22:31 +0700

    Revert "Attempt to remerge"

--- app/src/main/java/vn/nhh/aid/screens/PreventBack.kt
+++
@@ -1,8 +0,0 @@
-package vn.nhh.aid.screens
-
-import androidx.fragment.app.FragmentManager
-
-interface PreventBack {
-    fun handleBackPressed(fmanager: FragmentManager)
-    fun allowBack(): Boolean
-}
