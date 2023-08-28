8d1afe6a63e419abcf1435b4cb28a44295340e1d
Trung Nguyen Thien <ngthientrung@gmail.com>
2023-08-29 06:02:45 +0700

    no message

--- app/src/main/java/vn/nhh/aid/screens/lesson/LessonModel.kt
+++
@@ -1,9 +0,0 @@
-package vn.nhh.aid.screens.lesson
-
-class LessonModel(name: String, desc: String) {
-    var name: String? = null
-        get() = field
-
-    var desc: String? = null
-        get() = field
-}
